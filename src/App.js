653ea3be65343a6b366d2be2487aa52195fff1f4
Khushiomar <khushiomar2002@gmail.com>
2023-10-28 23:58:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-   <>
-  <Routes>
-    <Route path="/" element={<Home/>}/>
+  
+  <Routes >
+    <Route exact path="/React-Project-6/" element={<Home/>}/>
     <Route path="/about" element={<About/>}/>
     <Route path="/contact" element={<Contact/>}/>
     <Route path="/project" element={<Project/>}/>
     </Routes> 
-   </>
+  
+  
   );
 }
 
