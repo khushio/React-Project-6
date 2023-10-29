c3e3778dd55cde14a073d34d93249f78d89f447f
Khushiomar <khushiomar2002@gmail.com>
2023-10-29 05:51:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import Home from './routes/Home';
 import About from './routes/About';
 import Contact from './routes/Contact';
 import Project from './routes/Project';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return (
-  <BrowserRouter basename='/React-Project-6'>
-  <Routes >
+
+  <Routes  >
     
-    <Route exact path="/React-Project-6/" element={<Home/>}/>
+    <Route path="/React-Project-6" element={<Home/>}/>
     <Route path="/about" element={<About/>}/>
     <Route path="/contact" element={<Contact/>}/>
     <Route path="/project" element={<Project/>}/>
     </Routes> 
-    </ BrowserRouter>
+
   
   
   );
